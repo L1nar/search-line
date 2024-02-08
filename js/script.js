87864a8c0cba26dd8b823794cff8b06f8dd2fbda
L1nar <122251664+L1nar@users.noreply.github.com>
2024-02-08 12:26:35 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 let search = document.querySelector('#searchInput');
 let items = document.querySelectorAll('.item');
 
-let chunk = 'Груши';
+// строка.indexOf(подстрока);
+// Метод indexOf осуществляет поиск элемента в массиве.
+// Метод возвращает номер первого найденного элемента, либо -1, если такого элемента нет.
 
 search.addEventListener('keyup', ()=> {
+
+    // Получить данные из поля ввода и привести к нижнемы регистру
+    let chunk = search.value.toLowerCase();
+    
     for (let item of items) {
-        let str = item.textContent;
-        if (str.indexOf(search.value) != -1) {
-            console.log(str);
+
+        // Получить текстовое содержимое списка и привести к нижнему регистру
+        let str = item.textContent.toLowerCase();
+
+        // Если в списке найдены символы введенные с клавиатуры - вывести элемент списка на экран, иначе спрятать элемент списка
+        if (str.indexOf(chunk) != -1) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
         }
     }
 })
+
+ы
